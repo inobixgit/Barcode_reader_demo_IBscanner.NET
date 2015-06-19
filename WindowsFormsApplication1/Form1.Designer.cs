8caf4479664dd2ab8463f8f8709dad2de9f029b2
inobixgit <git@inobix.com>
2015-06-19 16:37:59 +0300

    first commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(114, 29);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load Image";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Image Files (*.bmp, *.jpg, *.gif, *.png)|*.bmp;*.jpg;*.gif;*.png";
+            // 
+            // tbResult
+            // 
+            this.tbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbResult.Location = new System.Drawing.Point(12, 47);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(291, 154);
+            this.tbResult.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(12, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "There are (*) added in the result because this is demo  version of IBscanner.NET " +
+    "Component";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 263);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Inobix.Windows.Components.IBscanner.Demo;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        BarcodeScanner ibscanner = new BarcodeScanner();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ibscanner.OnScanningCompleted += new BarcodeScanner.ScanningCompletedEventHandler(ibscanner_OnScanningCompleted); 
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            tbResult.Clear();
+            ibscanner.EnableMultiThreading = true;
+
+            string AppPath = Path.GetDirectoryName(Application.ExecutablePath);
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = Path.Combine(AppPath, "test_images");
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ibscanner.ReadFromFile(openFileDialog1.FileName);
+            }
+        }
+
+        void ibscanner_OnScanningCompleted()
+        {
+            foreach (BarcodeItem barcode in ibscanner.BarcodesList.Values)
+            {
+                // Get barcode value  
+                string codeValue = barcode.Barcode;
+
+                // Get barcode type  
+                string codeType = barcode.CodeType.ToString();
+
+                string coordinateX = barcode.CenterX.ToString();
+                string coordinateY = barcode.CenterY.ToString();
+                tbResult.AppendText("Value: " + codeValue + " Type: " + codeType + " X: " + coordinateX + " Y: " + coordinateY + Environment.NewLine);
+                
+            }
+        }  
+    }
+}
